3d1295813338da4b1b88b166c9ff16e17f066da3
aaron2012r2 <aaron.schroeder.12@gmail.com>
2017-07-18 07:56:20 -0500

    Token retrieval and storage
    Can now successfully obtain and store the token for continuations. It
    does not respect it at the moment.

--- R/cosmosQuery.R
+++ R/cosmosQuery.R
@@ -2,6 +2,8 @@
 #' 
 #' @param sql.what String for specifying what fields to retrieve. Typically called select condition. Defaults to *
 #' @param sql.where String for specifying what filter to use on data. Typically called search condition. Defaults to empty.
+#' @param max.items Number of documents to retrieve per page
+#' @param continuation Logical specifying whether to try to grab a next page of results
 #' @param debug.auth Logical value for getting verbose output of auth header being constructed. Defaults to false.
 #' @param debug.query Logical value for getting verbose output of HTTP response, printing all headers. Defaults to false.
 #' @param content.response Logical value to determine whether to retrieve full response or just the documents
@@ -11,7 +13,7 @@
 #' @examples
 #' cosmosQuery(sql.what = "c.contact.eloquaId", sql.where = "c.contact.eloquaId != null")
 
-cosmosQuery <- function(sql.what = "*", sql.where = "", max.items = 100, debug.auth = FALSE, debug.query = FALSE, content.response = FALSE) {
+cosmosQuery <- function(sql.what = "*", sql.where = "", max.items = 100, continuation = FALSE, debug.auth = FALSE, debug.query = FALSE, content.response = FALSE) {
 
     require(digest)
     require(base64enc)
@@ -44,7 +46,30 @@ cosmosQuery <- function(sql.what = "*", sql.where = "", max.items = 100, debug.a
 
     # Generate auth header using specifications
     auth.header <- genHeader(verb = "POST", resource.type = res.type, resource.link = res.link, stored.time = ms.date.string, debug = debug.auth)
-    raw.response <- POST(post.uri, add_headers(.headers = c("Authorization" = auth.header, "x-ms-version" = "2017-02-22", "x-ms-date" = ms.date.string, "Content-Type" = "application/query+json", "x-ms-documentdb-isquery" = "true", "x-ms-documentdb-query-enablecrosspartition" = "true", "x-ms-max-item-count" = max.items)), body = json.query)
+
+    # Assemble headers for the POST
+    all.headers <- c("Authorization" = auth.header, "x-ms-version" = "2017-02-22", "x-ms-date" = ms.date.string, "Content-Type" = "application/query+json", "x-ms-documentdb-isquery" = "true", "x-ms-documentdb-query-enablecrosspartition" = "true", "x-ms-max-item-count" = max.items)
+
+    # Check for continuation, if attempting to get pages
+    if (continuation == TRUE) {
+        if (!is.null(envCosmosDB$continuation$token)) {
+            continuation.token <- c("x-ms-continuation-token" = envCosmosDB$continuation$token)
+            all.headers <- append(all.headers, continuation.token)
+            print(all.headers)
+            rm(continuation, envir = envCosmosDB)
+        }
+    }
+
+    # Store the result of the call
+    raw.response <- POST(post.uri, add_headers(.headers = all.headers), body = json.query)
+
+    # Extract headers
+    response.headers <- raw.response$headers
+
+    # Extract continuation headers and token, store outside of function scope for next call
+    continuation.header <- response.headers$`x-ms-continuation`
+    continuation.header <- fromJSON(continuation.header)
+    assign("continuation", continuation.header, envir = envCosmosDB)
 
     # Send the status code of the POST to the console
     print(paste("Status Code is", raw.response$status_code, sep = " "))
